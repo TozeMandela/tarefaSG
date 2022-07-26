defaeb7d65f00fa3e794e560b6f4c47a6cdb61b3
Tozé Mandela <mandelajunior10@gmail.com>
2022-07-26 17:03:07 +0100

    pesquisando as tarefas apartir da pagina de gerenciamento

--- Lista-sg/public/javascripts/controller/id.js
+++ Lista-sg/public/javascripts/controller/id.js
@@ -1,5 +1,6 @@
 ((doc, win)=>{
 let id = localStorage.getItem('id');
+let select = doc.querySelector('.listTarefa');
 
 XhttpRequest.get(`/users/admin/add/tarefa/${id}`).then((data)=>{
     console.log('userTTTTTT: ',JSON.parse(data))
@@ -7,5 +8,23 @@ XhttpRequest.get(`/users/admin/add/tarefa/${id}`).then((data)=>{
     doc.querySelector('div.pull-left>p').innerHTML = JSON.parse(data)._name;
 
 })
+
+XhttpRequest.get(`/users`).then(users=>{
+    let arr = JSON.parse(users);
+
+    console.log('aa: ', users, select, arr)
+
+    arr.forEach(user => {
+
+        createOption(select, user._name, user._id);
+    });
+    
+});
+
+function createOption(se, op, value){
+
+    se.options[se.options.length] = new Option(op, value);
+
+}
 
 })(document, window);
